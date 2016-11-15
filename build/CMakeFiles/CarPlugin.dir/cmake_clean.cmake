84b6e89e5a38f46e161418d9c4883a46cdb0a514
Ruyi Shen <Ruyi@eduroam-165-3.dyn.carleton.edu>
2016-11-15 10:17:17 -0600

    Try to print hough lines - fail #1

---
+++ build/CMakeFiles/CarPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarPlugin.dir/CarPlugin.cc.o"
+  "libCarPlugin.pdb"
+  "libCarPlugin.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
