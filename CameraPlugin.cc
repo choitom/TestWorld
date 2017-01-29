177609e916b7b100939818232fa27f29e4949455
Baker <bakerk2@carleton.edu>
2017-01-29 15:10:16 -0600

    Added Ruyi's new CameraPlugin.cc

--- CameraPlugin.cc
+++ CameraPlugin.cc
@@ -2,82 +2,75 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/sensors/sensors.hh>
+#include <gazebo/rendering/rendering.hh>
 #include <gazebo/common/common.hh>
 #include <stdio.h>
 #include <vector>
 #include <iostream>
 #include <numeric>
-
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
-
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
-
 #include "CameraPlugin.hh"
-
 #define PI 3.14159265359
-
 using namespace gazebo;
 using namespace cv;
 using namespace std;
-
 // Register this plugin with the simulator
 GZ_REGISTER_SENSOR_PLUGIN(CameraPlugin)
-
 // Pointer to the update event connection
 event::ConnectionPtr updateConnection;
 sensors::MultiCameraSensorPtr parentSensor;
-
-
 void CameraPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
 {
-	this->parentSensor =
-		boost::dynamic_pointer_cast<sensors::MultiCameraSensor>(_sensor);
-
-	if(!this->parentSensor)
-	{
-		gzerr << "Couldn't find a camera\n";
-		return;
-	}
-
-	// connect to the sensor update event.
-	this->updateConnection = this->parentSensor->ConnectUpdated(boost::bind(&CameraPlugin::OnUpdate, this));
-
-	// make sure the parent sensor is active
-	this->parentSensor->SetActive(true);
+    this->parentSensor =
+        boost::dynamic_pointer_cast<sensors::MultiCameraSensor>(_sensor);
+    if(!this->parentSensor)
+    {
+        gzerr << "Couldn't find a camera\n";
+        return;
+    }
+    // connect to the sensor update event.
+    this->updateConnection = this->parentSensor->ConnectUpdated(boost::bind(&CameraPlugin::OnUpdate, this));
+    // make sure the parent sensor is active
+    this->parentSensor->SetActive(true);
 }
-
 void CameraPlugin::OnUpdate()
 {
-	// get image width, height and data
-	int width = (int)this->parentSensor->GetImageWidth(0);
-	int height = (int)this->parentSensor->GetImageHeight(0);
-	const unsigned char* imageData = this->parentSensor->GetImageData(0);
-
-	// create image matrix
-	Mat image = Mat(height, width, CV_8UC3, const_cast<unsigned char*>(imageData));
-	
-    
+    // get image width, height and data
+    int width = (int)this->parentSensor->GetImageWidth(0);
+    int height = (int)this->parentSensor->GetImageHeight(0);
+    const unsigned char* imageData = this->parentSensor->GetImageData(0);
+    // added about the viewport
+    int viewportWidth = (int)this->parentSensor->GetCamera(0)->GetViewportWidth();
+    int viewportHeight = (int)this->parentSensor->GetCamera(0)->GetViewportHeight();
+    cout << "viewportWidth pixels is " << viewportWidth << endl;
+    cout << "viewportHeight pixels is " << viewportHeight << endl;
+    math::Vector3 originCoord;
+    math::Vector3 direction;
+    this->parentSensor->GetCamera(0)->GetCameraToViewportRay(200, 200, originCoord, direction);
+    cout << "viewportWidth originCoord is " << originCoord << endl;
+    cout << "viewportWidth direction is " << direction << endl;
+    // double directionx = direction[0];
+    // double directiony = direction[1];
+    // double directionz = direction[2];
+    // create image matrix
+    Mat image = Mat(height, width, CV_8UC3, const_cast<unsigned char*>(imageData));
     // Rectangular region of interest
-    int ROI_lo = height/9;	
+    int ROI_lo = height/9;
     int ROI_hi = height/1.02;
-    
     Mat rect_roi(image.size(), image.type());
     image.copyTo(rect_roi);
     ROI(rect_roi, ROI_lo, ROI_hi);
-    
-    
     // Create sub ROIs
     int n_sub = 3;
     int interval = (ROI_hi-ROI_lo)/n_sub;
-    
     int sub_lo = ROI_lo;
     int sub_hi;
-    
     vector<Mat> subs;
     for(int i = 0; i < n_sub; i++)
     {
@@ -88,39 +81,31 @@ void CameraPlugin::OnUpdate()
         subs.push_back(sub);
         sub_lo = sub_hi;
     }
-    
     imshow("sub0", subs[0]);
     imshow("sub1", subs[1]);
     imshow("sub2", subs[2]);
-    
     // Process each sub ROI
     vector<Mat> proc_subs;
     for(size_t i = 0; i < subs.size(); i++)
     {
         proc_subs.push_back(preprocess(subs[i]));
     }
-    
-	
     // For each sub ROI, find vanishing point
-    vector<Point> pts;
+    vector<cv::Point> pts;
     for(size_t i = 0; i < proc_subs.size(); i++)
     {
         int lo = ROI_lo + i * interval;
         int hi = lo + interval;
-        Point pt = vanishPoint(proc_subs[i], lo);
+        cv::Point pt = vanishPoint(proc_subs[i], lo);
         pts.push_back(pt);
-        
         circle(image, pt, 2, Scalar(255,0,0), 3);
     }
-    
     imshow("img", image);
     imwrite("waypoints.png", image);
-    
     waitKey(4);
 }
-
 void CameraPlugin::ROI(Mat &mat, int lo, int hi)
-{  
+{
     for(size_t i = 0; i < mat.rows; i++)
     {
         if(i < lo || i > hi)
@@ -132,18 +117,15 @@ void CameraPlugin::ROI(Mat &mat, int lo, int hi)
                 mat.at<Vec3b>(i,j)[2] = 0;
             }
         }
-    }   
+    }
 }
-
-Point CameraPlugin::vanishPoint(Mat mat, int mid)
+cv::Point CameraPlugin::vanishPoint(Mat mat, int mid)
 {
     vector<Vec2f> lines;
     HoughLines(mat, lines, 1, PI/180, 38, 0, 0);
-    
     // inner most lines
     float rho_left = FLT_MAX, theta_left = FLT_MAX;
     float rho_right = FLT_MIN, theta_right = FLT_MIN;
-    
     for(size_t i = 0; i < lines.size(); i++)
     {
         float rho = lines[i][0], theta = lines[i][1];
@@ -167,42 +149,31 @@ Point CameraPlugin::vanishPoint(Mat mat, int mid)
             }
         }
     }
-    
     // convert polar to Cartesian
     double a1 = -(cos(theta_left)/sin(theta_left));
     double b1 = rho_left/sin(theta_left);
-    
     double a2 = -(cos(theta_right)/sin(theta_right));
     double b2 = rho_right/sin(theta_right);
-    
     // find intersection of two equations
     double x = (b1-b2)/(a2-a1);
     double y = a1 * x + b1;
-    
     double x2 = mat.cols/2;
     double y2 = mat.rows;
-    
     // y = mx + n, x = (y-n)/m
     double m = (y2-y)/(x2-x);
     double n = -m*x+y;
-    
     // find x, given y = mid
     int waypoint_x = (mid-n)/m;
-    
-    return Point(waypoint_x,mid);
+    return cv::Point(waypoint_x,mid);
 }
-
 // Gray -> Gaussian Blur -> Morph Open -> Edge
 Mat CameraPlugin::preprocess(Mat mat)
 {
     Mat gray, morph, canny;
     cvtColor(mat, gray, CV_BGR2GRAY);
-    GaussianBlur(gray, gray, Size(5,5), 0, 0);
-    
+    GaussianBlur(gray, gray, cv::Size(5,5), 0, 0);
     Mat erosion(5, 5, CV_8U, Scalar(1));
     morphologyEx(gray, morph, MORPH_OPEN, erosion);
-    
     Canny(gray, canny, 128, 255);
-    
     return canny;
 }
